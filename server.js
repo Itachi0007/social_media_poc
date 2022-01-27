804b5a97679e87361ea3313252c36e1b257eb8b1
Ahmer Mehmood <ahmer.mehmood191@gmail.com>
2022-01-27 17:23:40 +0530

    removed post controller content

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.get("/", (req, res) => {
 require("./routes")(app);
 
 // select the port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-	console.log(`Server started at ${port}...`);
+app.listen(process.env.PORT || 3000, function () {
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
